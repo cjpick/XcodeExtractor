2885e1c9c855baf9c08092d6fee9a30dfd7990b3
Christopher Jacob Pick <pick.cm@gmail.com>
2016-07-19 09:26:28 -0700

    Moved Element to it's own file.

---
+++ XcodeExtractor/Element.swift
@@ -0,0 +1,38 @@
+//
+//  Element.swift
+//  XcodeExtractor
+//
+//  Created by Christopher Pick on 7/19/16.
+//  Copyright © 2016 Christopher Pick. All rights reserved.
+//
+
+import Foundation
+
+
+public enum Element {
+    case segue(String)
+    case viewController(String)
+    case unknown
+}
+
+extension Element {
+    
+    public init(element:String, attributes:[String:String]) {
+        self = Element.createViewController(element: element, attributes: attributes) ?? Element.createSegue(element: element, attributes: attributes) ?? .unknown
+    }
+    
+    private static func createViewController(element:String, attributes:[String:String])->Element? {
+        if let customClass = attributes["customClass"] where attributes["sceneMemberID"] == "viewController" {
+            return .viewController(customClass)
+        }
+        return nil
+    }
+    
+    private static func createSegue(element:String, attributes:[String:String])->Element? {
+        if let id = attributes["identifier"] where element == "segue" {
+            return .segue(id)
+        }
+        return nil
+    }
+    
+}
--- XcodeExtractor/StoryboardParser.swift
+++ XcodeExtractor/StoryboardParser.swift
@@ -9,50 +9,6 @@
 import Foundation
 
 
-enum Element {
-    case segue(String)
-    case viewController(String)
-    case unknown
-}
-
-extension Element {
-    
-    init(element:String, attributes:[String:String]) {
-        self = Element.createViewController(element: element, attributes: attributes) ?? Element.createSegue(element: element, attributes: attributes) ?? .unknown
-        
-//        switch(element, attributes["sceneMemberID"], attributes["customClass"], attributes["identifier"]) {
-//        case ("segue", _, _, let id):
-//            if let segueId = id {
-//                self = .segue(segueId)
-//                return
-//            }
-//        case (_, "viewController"?, let custom, _):
-//            if let customClass = custom {
-//                self = .viewController(customClass)
-//                return
-//            }
-//        default:
-//            break
-//        }
-//        self = .unknown
-    }
-    
-    static func createViewController(element:String, attributes:[String:String])->Element? {
-        if let customClass = attributes["customClass"] where attributes["sceneMemberID"] == "viewController" {
-            return .viewController(customClass)
-        }
-        return nil
-    }
-    
-    static func createSegue(element:String, attributes:[String:String])->Element? {
-        if let id = attributes["identifier"] where element == "segue" {
-            return .segue(id)
-        }
-        return nil
-    }
-    
-}
-
 class StoryboardParser:NSObject, XMLParserDelegate {
     
     var data = SegueData()
